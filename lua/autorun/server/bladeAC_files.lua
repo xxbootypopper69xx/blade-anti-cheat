212058ef211bf0f377932684869272cc28129d7d
hack3r.64 <hack3r.64@gmail.com>
2011-03-11 22:45:22 +0000

    Added Open Source Code.

---
+++ lua/autorun/server/bladeAC_files.lua
@@ -0,0 +1,17 @@
+--
+-- Blade AntiCheat™
+--
+-- LUA based Cheat/Resource/Illegal CFG's/Convars/Aimbots/Hack detection.
+--
+-- Designed and Programed by: DarK_St3alth (St3alth Gaming)
+--
+-- More information: http://s3g.brainnerd.com/forum
+--
+--
+--
+--
+-- No files needed so far, just add like it would normaly be...
+--
+
+
+--resource.AddFile( "models/nova/airboat_seat.mdl" )
